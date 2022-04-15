fb89029a61c0373eb3732cdf9676bd6b1de11a7d
alicewuhhh <71121323+alicewuhhh@users.noreply.github.com>
2022-04-15 00:33:01 -0400

    dd

--- Script_preprocessing.m
+++ Script_preprocessing.m
@@ -5,8 +5,7 @@ S02=load('P01_S02_calibration.mat');
 
 % section find classes and codes
 codes=S01.calibration.stimcode;
-index=S01.calibration.stimpos;
-
+index=S01.calibration.stimpos
 
 eeg_class1=[];
 eeg_class2=[];
@@ -15,6 +14,7 @@ eeg_class3=[];
 ind_1=find(codes==131);
 ind_2=find(codes==132);
 ind_3=find(codes==133);
+
 for i=1:2
 eeg_class1=[eeg_class1,S01.calibration.data(4:17, index(ind_1(i)):index(ind_1(i)+1))];
 eeg_class2=[eeg_class2,S01.calibration.data(4:17, index(ind_2(i)):index(ind_2(i)+1))];
